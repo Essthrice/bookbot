edb68accbd42ba378a0553dee73a69c1fc28b6b3
Essthrice <devinmaynard301@gmail.com>
2025-11-21 00:10:22 -0500

    completed guided tutorial

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from stats import get_book_text, word_count, instance_count, sort_dict, print_report
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    
+    if len(sys.argv) != 2 :
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    
+    book_path = sys.argv[1]
     full_text = get_book_text(book_path)
     count = word_count(full_text)
     
