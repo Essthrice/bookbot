752af653e0ac7d39a901e0d65c2d9ecd0227c509
Essthrice <devinmaynard301@gmail.com>
2025-11-20 22:33:20 -0500

    completed first logging report

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from stats import get_book_text, word_count, instance_count
+from stats import get_book_text, word_count, instance_count, sort_dict, print_report
 
 def main():
-    full_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    full_text = get_book_text(book_path)
     count = word_count(full_text)
     
-    print(f"Found {count} total words")
 
-    instance_count(full_text)
+    chars = instance_count(full_text)
+    sorted_chars = sort_dict(chars)
+
+    print_report(sorted_chars,count,book_path)
+
 main()
--- stats.py
+++ stats.py
@@ -10,13 +10,14 @@ def word_count(text):
 
 def instance_count(text):
     #create uniform data
+    text = text.strip()
     text = text.lower()
+    text = text.replace(" ", "")
     
+
     intstance_dict = {}
     
     counter = 0
-    total_letters = len(text)
-    print(f"total letters in text = {total_letters}")
     for letter in text:
         counter += 1
         if letter in intstance_dict:
@@ -24,5 +25,44 @@ def instance_count(text):
         else:
             intstance_dict[letter]= 1
         
-    print(intstance_dict)
+    return intstance_dict
+
+
+def sort_on(item):
+    return item["num"]
+
+def sort_dict(dict):
+    list_of_chars = []
     
+    
+    for key, value in dict.items():
+        temp_dict1 = {}
+        temp_dict1["char"] = key
+        temp_dict1["num"] = value
+        list_of_chars.append(temp_dict1)
+    
+
+    list_of_chars.sort(reverse = True, key=sort_on)
+
+    return list_of_chars
+
+
+
+
+def print_report(text,word_count,book):
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at{book}" )
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for list in text:
+
+        char = list['char']
+        num = list['num']
+        if  char.isalpha():          
+            print(f"{char}: {num}")
+
+    print("============= END ===============")
+        
